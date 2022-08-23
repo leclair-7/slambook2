23f3e890c9074356d9875e3eef387ac7c9b40dbb
Lucas Hagel <lucas.hagel@disroot.org>
2022-08-22 22:50:24 -0600

    path

--- ch3/examples/plotTrajectory.cpp
+++ ch3/examples/plotTrajectory.cpp
@@ -8,7 +8,7 @@ using namespace std;
 using namespace Eigen;
 
 // path to trajectory file
-string trajectory_file = "./examples/trajectory.txt";
+string trajectory_file = "/mnt/lair/robots/lucas_slam/slambook2/ch3/examples/trajectory.txt";
 
 void DrawTrajectory(vector<Isometry3d, Eigen::aligned_allocator<Isometry3d>>);
 
